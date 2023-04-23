348b9f8840bd3c1c1013218cbbdb9dc83a690580
Eban-Covarrubias <130101682+Eban-Covarrubias@users.noreply.github.com>
2023-04-23 13:59:03 -0700

    Create ListTests.java

---
+++ ListTests.java
@@ -0,0 +1,34 @@
+import static org.junit.Assert.*;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.*;
+
+class StringChecker{
+    static boolean checkString(String checkthis) {
+        //uhh
+        return true;
+    }
+    
+}
+
+
+public class ListTests{
+
+
+    @Test 
+    public void testfilter(){
+
+    List<String> input1 = new ArrayList<String>();
+    input1.add("Hello");
+    input1.add("World");
+    input1.add("Java");
+
+    StringChecker sc = new StringChecker();
+    List<String> output = ListExamples.filter(input1, sc);
+    assertEquals(input1, output);
+	}
+
+}
